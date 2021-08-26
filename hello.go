7be9d2d4ce3e01340c0b564c1b1b6657fca7379e
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2021-08-26 15:58:48 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,67 +2,32 @@ package main
 
 import "fmt"
 
-type person struct {
-	firstName string `json:"firstName" yaml:"firstName"` // Tagging can be added for serialization
-	lastName  string
-	age       int
-}
-
 func main() {
-	//Assigning values to the fields in the person struct:
-	p1 := person{
-		firstName: "Carl",
-		lastName:  "Eschenbach",
-		age:       54,
-	}
-
-	fmt.Println("He is the man: ", p1)
-
-	type animal struct {
-		name            string
-		characteristics []string
-	}
-
-	animal1 := animal{
-		name: "Lion",
-		characteristics: []string{"Eats human",
-			"Wild Animal",
-			"King of the jungle",
+	// No name for this struct
+	// But we need to declare the type here
+	bio := struct {
+		firstName string
+		friends   map[string]int
+		favDrinks []string
+	}{
+		firstName: "Steven",
+		friends: map[string]int{
+			"Tim":   12345678,
+			"Abdul": 34789657,
+			"Kally": 28993332,
+		},
+		favDrinks: []string{
+			"Pepsi",
+			"Cocacola",
 		},
 	}
+	fmt.Println(bio.firstName)
 
-	//We use dot(.) to acces each field in the struct
-	fmt.Println("Animal name: ", animal1.name)
-	for _, v := range animal1.characteristics {
-		fmt.Printf("\t %v\n", v)
+	for k, v := range bio.friends {
+		fmt.Println(k, v)
 	}
 
-	// Nested Structs
-
-	//A herbivore is an animal, so it can have the animal struct as a field
-	type herbivore struct {
-		animal
-		eatHuman bool
-	}
-
-	herb := herbivore{
-		animal: animal{
-			name: "Goat",
-			characteristics: []string{"Lacks sense",
-				"Lazy",
-				"Eats grass",
-			},
-		},
-		eatHuman: false, //maybe
-	}
-
-	fmt.Println("\nThis animal:")
-	//We use dot(.) to acces each field in the struct
-	fmt.Println("Eats human? ", herb.eatHuman)
-	// Promotion
-	fmt.Println("Animal name:", herb.name) // <<====== No need for Animal!!
-	fmt.Println("Characteristics:")
-	for _, v := range herb.characteristics {
-		fmt.Printf("\t %v\n", v)
+	for k, v := range bio.favDrinks {
+		fmt.Println(k, v)
 	}
 }
