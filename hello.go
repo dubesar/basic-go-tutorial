2640c4c81bfe7bc5337bec36715051bc0a08c16a
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2021-08-26 09:57:25 +0530

    Update hello.go

--- hello.go
+++ hello.go
@@ -14,4 +14,29 @@ func main() {
 	y1 := 5
 	sum := x1 + y1
 	fmt.Println(sum)
+	
+	//printing
+	fmt.Println("Hello, World!")
+
+	//using variables
+	a := "hello"
+
+	//once a variable is defined it can used as
+	a = "world"
+
+	//type can be given to a variable as
+	var b string
+
+	//condition
+	if a == "hello" {
+		fmt.Println("a is hello")
+	} else if a == "world" {
+		fmt.Println("a is world")
+	} else {
+		fmt.Println("a is not hello")
+	}
+
+	if b == "" {
+		fmt.Println("do not keep any variable unused")
+	}
 }
